bdf5483408deba2d3a19dd4bc25d1a5f266504b3
jesus24e <jesusrodriguez65@aragon.unam.mx>
2025-04-03 11:33:18 -0600

    comentario borrado

--- src/Productos/Main.java
+++ src/Productos/Main.java
@@ -5,8 +5,6 @@ import Productos.Factory.productoFactory;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Tienda tiendaN = new Tienda();
